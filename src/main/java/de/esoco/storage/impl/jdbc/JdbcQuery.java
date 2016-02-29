bacf7317f71080c4ad4ed2f2076daedf586a306a
Elmar Sonnenschein <eso@esoco.de>
2016-02-29 14:38:30 +0100

    FRAMEWORK-158: Query for extra attributes causes assertion error
    Task-Url: http://pve-jira.lemarit.com:8080/browse/FRAMEWORK-158

--- src/main/java/de/esoco/storage/impl/jdbc/JdbcQuery.java
+++ src/main/java/de/esoco/storage/impl/jdbc/JdbcQuery.java
@@ -1,18 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// This file is a part of the 'esoco-storage' project.
-// Copyright 2015 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	  http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// esoco-storage source file
+// Copyright (c) 2016 by Elmar Sonnenschein / esoco GmbH
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.storage.impl.jdbc;
 
@@ -753,6 +741,9 @@ public class JdbcQuery<T> extends RelatedObject implements Query<T>, Closeable
 			if (fDetailAttr != null)
 			{
 				sDetailAttr = parseFunction(fDetailAttr);
+
+				// remove compare value that has been added by parseFunction()
+				aCompareAttributes.remove(aCompareAttributes.size() - 1);
 			}
 			else
 			{
