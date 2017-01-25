396ba8db0ec3a5d2544680cc55fc9e1a535216f7
Elmar Sonnenschein <eso@esoco.de>
2017-01-25 21:53:45 +0100

    Warnings removed

--- src/test/java/de/esoco/storage/AbstractStorageTest.java
+++ src/test/java/de/esoco/storage/AbstractStorageTest.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-storage' project.
-// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -186,7 +186,6 @@ public abstract class AbstractStorageTest
 	 *
 	 * @throws StorageException
 	 */
-	@SuppressWarnings("boxing")
 	@Test
 	public void testPaging() throws StorageException
 	{
