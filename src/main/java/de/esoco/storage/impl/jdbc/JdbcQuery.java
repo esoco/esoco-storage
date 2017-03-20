da2b5614f52347c771188fab9f067dfe54d13e6f
Elmar Sonnenschein <eso@esoco.de>
2017-03-20 10:52:27 +0100

    OPT: transfer QUERY_OFFSET and QUERY_LIMIT from query predicate to query

--- src/main/java/de/esoco/storage/impl/jdbc/JdbcQuery.java
+++ src/main/java/de/esoco/storage/impl/jdbc/JdbcQuery.java
@@ -1,12 +1,12 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-storage' project.
-// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//		 http://www.apache.org/licenses/LICENSE-2.0
+//	  http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -134,6 +134,16 @@ public class JdbcQuery<T> extends RelatedObject implements Query<T>, Closeable
 		// parseQueryCriteria will also fill aSortPredicates
 		sQueryCriteria = parseQueryCriteria(rMapping, pCriteria);
 		sOrderCriteria = createOrderCriteria(aSortPredicates);
+
+		if (pQuery.hasRelation(QUERY_OFFSET))
+		{
+			set(QUERY_OFFSET, pQuery.get(QUERY_OFFSET));
+		}
+
+		if (pQuery.hasRelation(QUERY_LIMIT))
+		{
+			set(QUERY_LIMIT, pQuery.get(QUERY_LIMIT));
+		}
 
 		if (pQuery.hasRelation(QUERY_DEPTH))
 		{
